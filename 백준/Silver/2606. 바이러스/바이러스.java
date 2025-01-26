61e91457c7c2aa468db7a706c3b57e520245cc7d
sea5812 <150684333+sea5812@users.noreply.github.com>
2025-01-26 21:25:27 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,41 +2,42 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static int[] input;
-    private static ArrayList<Integer>[] adjList;
-    private static int[] visited;
-    private static int cnt;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
+    static int n, m, ans;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
 
-        adjList = new ArrayList[n+1];
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
         for (int i = 0; i <= n; i++) {
-            adjList[i] = new ArrayList<>();
+            graph.add(new ArrayList<>());
         }
-
+        visited = new boolean[n + 1];
         for (int i = 0; i < m; i++) {
-            input = Arrays.stream(br.readLine().split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-            adjList[input[0]].add(input[1]);
-            adjList[input[1]].add(input[0]);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
 
-        visited = new int[n+1];
-        dfs(1);
-        System.out.println(cnt);
+        DFS(1);
+        System.out.println(ans - 1);
     }
-    private static void dfs(int start) {
-        visited[start] = 1;
-        for (int next : adjList[start]) {
-            if (visited[next] == 0) {
-                cnt++;
-                dfs(next);
+
+    static void DFS(int u) {
+        visited[u] = true;
+        ans++;
+        for (int next : graph.get(u)) {
+            if (!visited[next]) {
+                DFS(next);
             }
         }
     }
