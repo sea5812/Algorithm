91475580b4f73ebfb75a179f3f28facabc3c2372
sea5812 <150684333+sea5812@users.noreply.github.com>
2025-01-28 01:54:47 +0900

    [Gold V] Title: 토마토, Time: 592 ms, Memory: 129588 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] board;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int m, n, ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        board = new int[n][m];
+        visited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        BFS();
+        System.out.println(ans);
+    }
+
+    static void BFS() {
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 1) {
+                    q.add(new int[]{i, j, 0});
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (isValid(nx, ny)) {
+                    q.offer(new int[]{nx, ny, cur[2] + 1});
+                    visited[nx][ny] = true;
+                    ans = Math.max(ans, cur[2] + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j] && board[i][j] == 0) {
+                    ans = -1;
+                    break;
+                }
+            }
+        }
+    }
+
+    static boolean isValid(int x, int y) {
+        return x >= 0 && x < n && y >= 0 && y < m && !visited[x][y] && board[x][y] == 0;
+    }
+}
