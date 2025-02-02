536f569e4516a43e9e8b6b8c00873a2d2d6b1965
sea5812 <150684333+sea5812@users.noreply.github.com>
2025-02-02 21:39:48 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 568 ms, Memory: 58256 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -1,27 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
+    static int n, m;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String[] input = br.readLine().split(" ");
-        int N = Integer.parseInt(input[0]);
-        int M = Integer.parseInt(input[1]);
-
-        Map<String, String> hm = new HashMap<>();
-        while (N-- > 0) {
-            input = br.readLine().split(" ");
-            hm.put(input[0], input[1]);
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        HashMap<String, String> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            map.put(st.nextToken(), st.nextToken());
         }
-        while (M-- > 0) {
-            sb.append(hm.get(br.readLine())).append("\n");
+        for (int i = 0; i < m; i++) {
+            sb.append(map.get(br.readLine())).append('\n');
         }
         System.out.println(sb);
     }
 }
-
