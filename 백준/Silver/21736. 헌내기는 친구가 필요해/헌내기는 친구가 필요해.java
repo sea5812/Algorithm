2fd097160ab5df0ca4f80e6dde69c06cb7039680
sea5812 <150684333+sea5812@users.noreply.github.com>
2025-02-04 23:05:14 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 256 ms, Memory: 39160 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,15 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
     static char[][] board;
     static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int n, m, x, y;
+    static int n, m, x, y, ans;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,7 +26,7 @@ public class Main {
                 }
             }
         }
-        int ans = BFS();
+        DFS(x, y);
         if (ans == 0) {
             System.out.println("TT");
         } else {
@@ -36,25 +34,15 @@ public class Main {
         }
     }
 
-    static int BFS() {
-        int cnt = 0;
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{x, y});
-        visited[x][y] = true;
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int x = cur[0];
-            int y = cur[1];
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny] && board[nx][ny] != 'X') {
-                    q.offer(new int[]{nx, ny});
-                    visited[nx][ny] = true;
-                    if (board[nx][ny] == 'P') cnt++;
-                }
+    static void DFS(int r, int c) {
+        visited[r][c] = true;
+        for (int i = 0; i < 4; i++) {
+            int nx = r + dx[i];
+            int ny = c + dy[i];
+            if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny] && board[nx][ny] != 'X') {
+                if (board[nx][ny] == 'P') ans++;
+                DFS(nx, ny);
             }
         }
-        return cnt;
     }
 }
