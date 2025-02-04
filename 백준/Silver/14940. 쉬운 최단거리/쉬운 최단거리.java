e7a6ccb7c6d05ce8ec99dc1ff4197e95fd8ce7f1
sea5812 <150684333+sea5812@users.noreply.github.com>
2025-02-04 20:52:08 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 644 ms, Memory: 51756 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[][] board;
+    static int[][] dist;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int n, m, x, y;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        board = new int[n][m];
+        dist = new int[n][m];
+
+        for (int[] rows : dist) {
+            Arrays.fill(rows, -1);
+        }
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+                if (board[i][j] == 2) {
+                    x = i; y = j;
+                }
+            }
+        }
+        BFS();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 0) {
+                    sb.append(0).append(" ");
+                } else {
+                    sb.append(dist[i][j]).append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void BFS() {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x, y});
+        dist[x][y] = 0;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m && dist[nx][ny] == -1 && board[nx][ny] == 1) {
+                    q.offer(new int[]{nx, ny});
+                    dist[nx][ny] = dist[x][y] + 1;
+                }
+            }
+        }
+    }
+}
